703e99a11d7360aa8e0286f3532f1078a1fdf023
shamoda <it19161648@my.sliit.lk>
2021-02-12 16:57:25 +0530

    Initial commit

---
+++ src/main/java/com/practicaltest/countriesapp/CountriesappApplication.java
@@ -0,0 +1,13 @@
+package com.practicaltest.countriesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicaltest/countriesapp/CountriesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicaltest.countriesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
